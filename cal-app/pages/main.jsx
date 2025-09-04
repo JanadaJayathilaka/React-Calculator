0710b65f6ccec59b9339e7e02d52a9d92f2a6ffc
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-09-04 16:38:09 +0530

    feat: enhance calculator functionality and input handling

--- cal-app/pages/main.jsx
+++ cal-app/pages/main.jsx
@@ -1,49 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import { Divide } from "lucide-react";
 import { Radical } from "lucide-react";
 import { Delete } from "lucide-react";
 import { X } from "lucide-react";
 function Main() {
+  const [input, setInput] = useState("");
+
+  const handleButtonClick = (value) => {
+    setInput((prev) => prev + value);
+  };
   return (
     <>
       <div className="bg-cyan-500 pt-10 w-full h-screen">
         <div className="flex items-center flex-col ">
           <div className="flex flex-col text-2xl font-bold text-gray-800 p-4 bg-gray-700 ">
             <div className="text-center text-white">
-              <h1>Calculator</h1>
+              <h1 className="text-orange-600">Calculator</h1>
             </div>
             <div className="bg-gray-700 m-2.5 border-2 border-gray-700">
               <input
-                type="number"
+                type="text"
                 className="p-2 w-full text-white bg-gray-700 rounded-lg focus:outline-none"
                 placeholder="0"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
               />
             </div>
             <div className="flex items-center justify-between">
               <button className="bg-gray-600 text-orange-600 p-2 rounded-xl align-baseline w-15 h-15">
                 <Radical className="w-8 h-8 font-bold" />
               </button>
-              <button className="bg-gray-600 text-orange-600 p-2 rounded-xl align-baseline w-15 h-15">
-                AC
+              <button
+                onClick={() => {
+                  setInput("");
+                }}
+                className="bg-gray-600 text-orange-600 p-2 rounded-xl align-baseline w-15 h-15"
+              >
+                {input ? "C" : "AC"}
               </button>
               <button className="flex items-center justify-center bg-gray-600 text-orange-600 p-2 rounded-xl align-baseline ml-0.5 w-15 h-15">
                 <Delete className="font-bold" />
               </button>
-              <button className="flex items-center justify-center bg-gray-600 text-orange-600 p-2 rounded-xl align-baseline ml-0.5 w-15 h-15">
+              <button
+                className="flex items-center justify-center bg-gray-600 text-orange-600 p-2 rounded-xl align-baseline ml-0.5 w-15 h-15"
+                onClick={() => {
+                  handleButtonClick("(");
+                }}
+              >
                 (
               </button>
-              <button className="flex items-center justify-center bg-gray-600 text-orange-600 p-2 rounded-xl align-baseline ml-0.5 w-15 h-15">
+              <button
+                className="flex items-center justify-center bg-gray-600 text-orange-600 p-2 rounded-xl align-baseline ml-0.5 w-15 h-15"
+                onClick={() => {
+                  handleButtonClick(")");
+                }}
+              >
                 )
               </button>
             </div>
             <div className="flex items-center justify-between mt-2">
-              <button className="bg-gray-600 text-white p-2 rounded-xl align-baseline w-15 h-15">
+              <button
+                className="bg-gray-600 text-white p-2 rounded-xl align-baseline w-15 h-15"
+                onClick={() => {
+                  handleButtonClick("7");
+                }}
+              >
                 7
               </button>
-              <button className="bg-gray-600 text-white p-2 rounded-xl align-baseline w-15 h-15">
+              <button
+                className="bg-gray-600 text-white p-2 rounded-xl align-baseline w-15 h-15"
+                onClick={() => {
+                  handleButtonClick("8");
+                }}
+              >
                 8
               </button>
-              <button className="flex items-center justify-center bg-gray-600 text-white p-2 rounded-xl align-baseline ml-0.5 w-15 h-15">
+              <button
+                className="flex items-center justify-center bg-gray-600 text-white p-2 rounded-xl align-baseline ml-0.5 w-15 h-15"
+                onClick={() => {
+                  handleButtonClick("9");
+                }}
+              >
                 9
               </button>
               <button className="bg-gray-600 text-orange-600 p-2 rounded-xl flex items-center justify-center ml-0.5 w-15 h-15">
@@ -54,13 +91,28 @@ function Main() {
               </button>
             </div>
             <div className="flex items-center justify-between mt-2">
-              <button className="bg-gray-600 text-white p-2 rounded-xl align-baseline w-15 h-15">
+              <button
+                className="bg-gray-600 text-white p-2 rounded-xl align-baseline w-15 h-15"
+                onClick={() => {
+                  handleButtonClick("4");
+                }}
+              >
                 4
               </button>
-              <button className="bg-gray-600 text-white p-2 rounded-xl align-baseline w-15 h-15">
+              <button
+                className="bg-gray-600 text-white p-2 rounded-xl align-baseline w-15 h-15"
+                onClick={() => {
+                  handleButtonClick("5");
+                }}
+              >
                 5
               </button>
-              <button className="flex items-center justify-center bg-gray-600 text-white p-2 rounded-xl align-baseline ml-0.5 w-15 h-15">
+              <button
+                className="flex items-center justify-center bg-gray-600 text-white p-2 rounded-xl align-baseline ml-0.5 w-15 h-15"
+                onClick={() => {
+                  handleButtonClick("6");
+                }}
+              >
                 6
               </button>
               <button className="bg-gray-600 text-orange-600 p-2 rounded-xl flex items-center justify-center ml-0.5 w-15 h-15">
@@ -71,13 +123,28 @@ function Main() {
               </button>
             </div>
             <div className="flex items-center justify-between mt-2">
-              <button className="bg-gray-600 text-white p-2 rounded-xl align-baseline w-15 h-15">
+              <button
+                className="bg-gray-600 text-white p-2 rounded-xl align-baseline w-15 h-15"
+                onClick={() => {
+                  handleButtonClick("1");
+                }}
+              >
                 1
               </button>
-              <button className="bg-gray-600 text-white p-2 rounded-xl align-baseline w-15 h-15">
+              <button
+                className="bg-gray-600 text-white p-2 rounded-xl align-baseline w-15 h-15"
+                onClick={() => {
+                  handleButtonClick("2");
+                }}
+              >
                 2
               </button>
-              <button className="flex items-center justify-center bg-gray-600 text-white p-2 rounded-xl align-baseline ml-0.5 w-15 h-15">
+              <button
+                className="flex items-center justify-center bg-gray-600 text-white p-2 rounded-xl align-baseline ml-0.5 w-15 h-15"
+                onClick={() => {
+                  handleButtonClick("3");
+                }}
+              >
                 3
               </button>
               <button className="bg-gray-600 text-orange-600 p-2 rounded-xl flex items-center justify-center ml-0.5 w-15 h-15">
@@ -88,7 +155,12 @@ function Main() {
               </button>
             </div>
             <div>
-              <button className="bg-gray-600 text-white p-2 rounded-xl  h-15 mt-2 lg:w-52 md:w-45 sm:flex-2 w-full">
+              <button
+                className="bg-gray-600 text-white p-2 rounded-xl  h-15 mt-2 lg:w-52 md:w-45 sm:flex-2 w-full"
+                onClick={() => {
+                  handleButtonClick("0");
+                }}
+              >
                 0
               </button>
             </div>
